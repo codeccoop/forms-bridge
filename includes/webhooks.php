9ca817edd0c657da02083e3d9b20a19d5c7a8790
Lucas García <lucas@codeccoop.org>
2023-10-10 14:20:25 +0200

    feat: soruce_xml_id as optional form field

--- includes/webhooks.php
+++ includes/webhooks.php
@@ -3,7 +3,13 @@ add_action('gform_after_submission', 'wpct_crm_forms_api_submissions', 10, 2);
 function wpct_crm_forms_api_submissions($entry, $form)
 {
 	$form_vals = wpct_crm_forms_parse_form_entry($entry, $form);
-	if (!isset($form_vals['source_xml_id'])) throw new Exception('Error Processing Request', 400);
+
+	if (!isset($form_vals['source_xml_id'])) {
+		$form_id = rgat($form, 'id');
+		$form_title = rgar($form, 'title');
+		$form_vals['source_xml_id'] = "{$form_id}_{$form_title}";
+	}
+
 	$submission_payload = apply_filters('wpct_crm_forms_prepare_submission', $form_vals);
 
 	$response = wpct_oc_post_odoo('/api/private/crm-lead', $submission_payload);
