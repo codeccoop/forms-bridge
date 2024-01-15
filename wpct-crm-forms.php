e9b322d898268ff0fe1e569949922b92d10f3c44
Pau <pau.pulidocompanys@gmail.com>
2024-01-15 12:03:34 +0100

    feat: increase plugin version

--- wpct-crm-forms.php
+++ wpct-crm-forms.php
@@ -8,7 +8,7 @@
  * Author URI:      https://www.codeccoop.org
  * Text Domain:     wpct-crm-forms
  * Domain Path:     /languages
- * Version:         1.0.2
+ * Version:         1.0.3
  *
  * @package         WPCT_CRM_Forms
  */
