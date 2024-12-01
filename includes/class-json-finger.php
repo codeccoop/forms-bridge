9474510c1ae68da133b90ac07ef4f0e955ce0f52
Lucas García <lucas@codeccoop.org>
2024-12-01 11:38:44 +0100

    feat: json finger unset method and docstrings

--- includes/class-json-finger.php
+++ includes/class-json-finger.php
@@ -6,101 +6,26 @@ use ValueError;
 use TypeError;
 use Error;
 
+/**
+ * JSON Finger handler.
+ */
 class JSON_Finger
 {
+    /**
+     * Handle target array data.
+     *
+     * @var array $data Target array data.
+     */
     private $data;
 
-    public function __construct($data)
-    {
-        if (!is_array($data)) {
-            throw new TypeError('Input data isn\'t an array');
-        }
-
-        $this->data = $data;
-    }
-
-    public function __get($name)
-    {
-        if (isset($this->data[$name])) {
-            return $this->data[$name];
-        }
-    }
-
-    public function __set($name, $value)
-    {
-        $this->data[$name] = $value;
-    }
-
-    public function data()
-    {
-        return $this->data;
-    }
-
-    public function get($finger)
-    {
-        if ($this->isset($finger)) {
-            return $this->$finger;
-        }
-
-        $value = null;
-        try {
-            $keys = $this->parse($finger);
-
-            $value = $this->data;
-            foreach ($keys as $key) {
-                if (!isset($value[$key])) {
-                    $value = null;
-                    break;
-                }
-                $value = $value[$key];
-            }
-        } catch (Error $e) {
-            error_log($e->getMessage());
-            return null;
-        }
-
-        return $value;
-    }
-
-    public function set($finger, $value, $unset = false)
-    {
-        if ($this->isset($finger)) {
-            $this->$finger = $value;
-        }
-
-        $data = $this->data;
-        try {
-            $keys = $this->parse($finger);
-            $partial = &$data;
-            for ($i = 0; $i < count($keys) - 1; $i++) {
-                $key = $keys[$i];
-                if (!isset($partial[$key])) {
-                    $partial[$key] = [];
-                }
-                $partial = &$partial[$key];
-            }
-
-            $key = $keys[$i];
-            if ($unset) {
-                unset($partial[$key]);
-            } else {
-                $partial[$key] = $value;
-            }
-        } catch (Error $e) {
-            error_log($e->getMessage());
-            return null;
-        }
-
-        $this->data = $data;
-        return $data;
-    }
-
-    public function unset($finger)
-    {
-        return $this->set($finger, null, true);
-    }
-
-    private function parse($finger)
+    /**
+     * Parse a json finger and returns it as an array of keys.
+     *
+     * @param string $finger JSON finger.
+     *
+     * @return array Array with finger keys.
+     */
+    public static function parse($finger)
     {
         $len = strlen($finger);
         $keys = [];
@@ -160,8 +85,160 @@ class JSON_Finger
         return $keys;
     }
 
-    public function isset($name)
+    /**
+     * Binds data to the handler instance.
+     *
+     * @param array $data Target data.
+     */
+    public function __construct($data)
     {
-        return isset($this->data[$name]);
+        if (!is_array($data)) {
+            throw new TypeError('Input data isn\'t an array');
+        }
+
+        $this->data = $data;
+    }
+
+    /**
+     * Proxy handler attributes to the data.
+     *
+     * @param string $name Attribute name.
+     *
+     * @return mixed Attribute value or null.
+     */
+    public function __get($name)
+    {
+        if (isset($this->data[$name])) {
+            return $this->data[$name];
+        }
+    }
+
+    /**
+     * Proxy handler attribute updates to the data.
+     *
+     * @param string $name Attribute name.
+     * @param mixed $value Attribute value.
+     */
+    public function __set($name, $value)
+    {
+        $this->data[$name] = $value;
+    }
+
+    /**
+     * Returns de current data.
+     *
+     * @return array Current data.
+     */
+    public function data()
+    {
+        return $this->data;
+    }
+
+    /**
+     * Gets the attribute from the data.
+     *
+     * @param string $finger JSON finger.
+     *
+     * @return mixed Attribute value.
+     */
+    public function get($finger)
+    {
+        if ($this->$finger) {
+            return $this->$finger;
+        }
+
+        $value = null;
+        try {
+            $keys = static::parse($finger);
+
+            $value = $this->data;
+            foreach ($keys as $key) {
+                if (!isset($value[$key])) {
+                    $value = null;
+                    break;
+                }
+                $value = $value[$key];
+            }
+        } catch (Error $e) {
+            error_log($e->getMessage());
+            return null;
+        }
+
+        return $value;
+    }
+
+    /**
+     * Sets the attribute value on the data.
+     *
+     * @param string $finger JSON finger.
+     * @param mixed $value Attribute value.
+     * @param boolean $unset If true, unsets the attribute.
+     *
+     * @return array Data after the attribute update.
+     */
+    public function set($finger, $value, $unset = false)
+    {
+        if ($this->$finger) {
+            $this->$finger = $value;
+        }
+
+        $data = $this->data;
+        $breadcrumb = [];
+        try {
+            $keys = static::parse($finger);
+            $partial = &$data;
+            for ($i = 0; $i < count($keys) - 1; $i++) {
+                $key = $keys[$i];
+                if (!isset($partial[$key])) {
+                    $partial[$key] = [];
+                }
+                $breadcrumb[] = ['partial' => &$partial, 'key' => $key];
+                $partial = &$partial[$key];
+            }
+
+            $key = $keys[$i];
+            if ($unset) {
+                unset($partial[$key]);
+                for ($i = count($breadcrumb) - 1; $i >= 0; $i--) {
+                    $step = $breadcrumb[$i];
+                    $partial = &$step['partial'];
+                    $key = $step['key'];
+                    if (!empty($partial[$key])) {
+                        break;
+                    }
+                    unset($partial[$key]);
+                }
+            } else {
+                $partial[$key] = $value;
+            }
+        } catch (Error $e) {
+            error_log($e->getMessage());
+            return null;
+        }
+
+        $this->data = $data;
+        return $data;
+    }
+
+    /**
+     * Unsets the attribute from the data.
+     *
+     * @param string $finger JSON finger.
+     */
+    public function unset($finger)
+    {
+        return $this->set($finger, null, true);
+    }
+
+    /**
+     * Checks if the json finger is set on the data.
+     *
+     * @param string $finger JSON finger.
+     *
+     * @return boolean True if attribute is set.
+     */
+    public function isset($finger)
+    {
+        return !empty($this->get($finger));
     }
 }
