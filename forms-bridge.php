607fb4e33d83bc052bd22f19835ade5b361e3952
Lucas García <lucas@codeccoop.org>
2024-12-21 03:46:12 +0100

    feat: conditionally load composer autoload

--- forms-bridge.php
+++ forms-bridge.php
@@ -8,7 +8,7 @@ Author:          Còdec
 Author URI:      https://www.codeccoop.org
 Text Domain:     forms-bridge
 Domain Path:     /languages
-Version:         2.1.0
+Version:         2.1.1
 */
 
 namespace FORMS_BRIDGE;
