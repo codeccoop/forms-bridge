604bcd92f7a8f57c9c19d90388f9fc422db6aaa3
Lucas García <lucas@codeccoop.org>
2025-03-16 20:20:44 +0100

    feat: addon loading optimization

--- forms-bridge.php
+++ forms-bridge.php
@@ -75,7 +75,13 @@ class Forms_Bridge extends Base_Plugin
     {
         parent::construct(...$args);
 
-        Addon::load();
+        if (
+            Menu::is_admin_current_page() ||
+            REST_Settings_Controller::is_doing_rest()
+        ) {
+            Addon::load();
+        }
+
         Integration::load();
 
         self::http_hooks();
@@ -243,6 +249,8 @@ class Forms_Bridge extends Base_Plugin
      */
     public static function do_submission()
     {
+        Addon::lazy_load();
+
         $form_data = apply_filters('forms_bridge_form', null);
         if (!$form_data) {
             return;
