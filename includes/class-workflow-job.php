06d382a28ba7cd93e9f4b15c3951e2ae4171229d
Lucas García <lucas@codeccoop.org>
2025-03-31 02:44:22 +0200

    feat: input workflow job mutations

--- includes/class-workflow-job.php
+++ includes/class-workflow-job.php
@@ -203,10 +203,14 @@ class Workflow_Job
             return $payload;
         }
 
+        $mutation = array_shift($mutations) ?: [];
+        $payload = $bridge->apply_mutation($payload, $mutation);
+
         $payload = apply_filters(
             'forms_bridge_workflow_job_payload',
             $payload,
-            $this
+            $this,
+            $bridge
         );
 
         $method = $this->method;
@@ -221,9 +225,6 @@ class Workflow_Job
         }
 
         $payload = $this->output_payload($payload);
-
-        $mutation = array_shift($mutations) ?: [];
-        $payload = $bridge->apply_mutation($payload, $mutation);
 
         if ($next_job = $this->next) {
             $payload = $next_job->run($payload, $bridge, $mutations);
