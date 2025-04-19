040405d13ed6b6aa35a615bbde99c574b41b00d3
Lucas García <lucas@codeccoop.org>
2025-04-19 16:53:38 +0200

    feat: useCurrentApi hook

---
+++ src/hooks/useCurrentApi.js
@@ -0,0 +1,26 @@
+const { useEffect, useState, useRef } = wp.element;
+
+export default function useCurrentApi() {
+  const [api, setApi] = useState(null);
+
+  const onApi = useRef((api) => {
+    console.log(api);
+    setApi(api);
+  }).current;
+
+  useEffect(() => {
+    const tab = new URLSearchParams(window.location.search).get("tab");
+
+    if (tab !== "general") {
+      setApi(tab);
+    }
+
+    wpfb.on("api", onApi);
+
+    return () => {
+      wpfb.off("api", onApi);
+    };
+  }, []);
+
+  return api;
+}
