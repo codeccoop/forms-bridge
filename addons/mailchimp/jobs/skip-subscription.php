8883555aa72e08189519d9eae1dd50d1207c90e7
Lucas García <lucas@codeccoop.org>
2025-05-06 19:04:07 +0200

    feat: skip subscriptions jobs

---
+++ addons/mailchimp/jobs/skip-subscription.php
@@ -0,0 +1,29 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit();
+}
+
+return [
+    'title' => __('Skip subscription', 'forms-bridge'),
+    'description' => __(
+        'Skip subscription if the mailchimp field is not true',
+        'forms-bridge'
+    ),
+    'method' => 'forms_bridge_mailchimp_skip_subscription',
+    'input' => [
+        [
+            'name' => 'mailchimp',
+            'schema' => ['type' => 'boolean'],
+            'required' => true,
+        ],
+    ],
+    'output' => [],
+];
+
+function forms_bridge_mailchimp_skip_subscription($payload)
+{
+    if ($payload['mailchimp'] === true) {
+        return $payload;
+    }
+}
