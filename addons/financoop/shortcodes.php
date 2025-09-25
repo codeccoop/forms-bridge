baad633f9986e24a9a0ffd83801b7bef2f609d29
Lucas García <lucas@codeccoop.org>
2025-09-25 15:00:48 +0200

    feat: financoop campaign shortcode

--- addons/financoop/shortcodes.php
+++ addons/financoop/shortcodes.php
@@ -14,14 +14,37 @@ add_shortcode('financoop_campaign', function ($atts) {
     require_once $addon_dir . '/class-financoop-addon.php';
 
     if (!isset($atts['id'])) {
-        return "[financoop_campaign id='null']Missing 'id' param[/financoop_campaign]";
+        return forms_bridge_financoop_shortcode_error(
+            'Missing "id" param',
+            $atts
+        );
     }
 
     if (!isset($atts['backend'])) {
-        return "[financoop_campaign id='{$atts['id']}' backend='null']Missing 'backend' param[/financoop_campaign]";
+        return forms_bridge_financoop_shortcode_error(
+            'Missing "backend" param',
+            $atts
+        );
     }
 
-    $currency = $atts['currency'] ?? '€';
+    if (!isset($atts['sources'])) {
+        $atts['sources'] = ['global'];
+    } else {
+        $valid_sources = ['global', 'subscription', 'donation', 'loan'];
+        $sources = array_map(
+            'strtolower',
+            array_map('trim', explode(',', $atts['sources']))
+        );
+
+        $atts['sources'] = [];
+        foreach ($sources as $source) {
+            if (in_array($source, $valid_sources, true)) {
+                $atts['sources'][] = $source;
+            }
+        }
+    }
+
+    $atts['currency'] = $atts['currency'] ?? '€';
 
     try {
         $bridge = new Finan_Coop_Form_Bridge(
@@ -36,21 +59,24 @@ add_shortcode('financoop_campaign', function ($atts) {
 
         $response = $bridge->submit();
         if (is_wp_error($response)) {
-            $message = $campaign->get_error_message();
-            return "[financoop_campaign id='{$atts['id']}' backend='{$atts['backend']}']Fetch campaign error message: {$message}[/financoop_campaign]";
+            $message = $response->get_error_message();
+            return forms_bridge_financoop_shortcode_error(
+                "Fetch campaign error message: {$message}",
+                $atts
+            );
         }
 
         $campaign = $response['data'];
     } catch (Error | Exception $e) {
-        return "[financoop_campaign id='{$atts['id']}' backend='{$atts['backend']}']Fetch campaign error message: {$e->getMessage()}[/financoop_campaign]";
+        return forms_bridge_financoop_shortcode_error(
+            "Fetch campaign error message: {$e->getMessage()}",
+            $atts
+        );
     }
 
     ob_start();
-    ?><style>.financoop-campaign-state{background:var(--wp-admin-theme-color);color:white;font-size:0.8em;padding:0 0.6em;width:fit-content;border-radius:1em}
-.financoop-campaign-dates{padding-left:0;list-style:none;}
-.financoop-progress>p{margin:0}</style>
-<article class="financoop-campaign wp-block-group">
-    <div class="finanacoop-campaign-header wp-block-group">
+    ?><article class="financoop-campaign wp-block-group">
+    <div class="financoop-campaign-header">
         <div class="financoop-campaign-state"><?php echo esc_html(
             $campaign['state']
         ); ?></div>
@@ -62,46 +88,65 @@ add_shortcode('financoop_campaign', function ($atts) {
         <?php endif; ?>
     </div>
     <div class="financoop-campaign-content">
+        <?php echo financoop_render_campaign_progress($campaign, $atts); ?>
         <?php echo financoop_render_campaign_dates($campaign); ?>
-        <?php echo financoop_render_campaign_progress($campaign, $currency); ?>
     </div>
 </article><?php
-$output = ob_get_clean();
+$output = shortcode_unautop(ob_get_clean());
+
 return apply_filters(
     'forms_bridge_financoop_campaign_html',
     $output,
-    $campaign
+    $campaign,
+    $atts
 );
 });
 
-function financoop_render_campaign_progress($campaign, $currency)
+function financoop_render_campaign_progress($campaign, $atts)
 {
-    $output = financoop_render_source_progress(
-        'global',
-        $campaign['global_objective'],
-        $campaign['progress'],
-        $currency
-    );
+    $output = '';
 
-    $sources = ['subscription', 'loan', 'donation'];
-    foreach ($sources as $source) {
+    if (in_array('global', $atts['sources'], true)) {
+        $output = financoop_render_source_progress(
+            $campaign,
+            'global',
+            $campaign['global_objective'],
+            $campaign['progress'],
+            $atts['currency']
+        );
+    }
+
+    foreach ($atts['sources'] as $source) {
         if ($campaign["has_{$source}_source"]) {
             $output .= financoop_render_source_progress(
+                $campaign,
                 $source,
                 $campaign["source_objective_{$source}"],
                 $campaign["progress_{$source}"],
-                $currency
+                $atts['currency']
             );
         }
     }
 
-    return $output;
+    return apply_filters(
+        'forms_bridge_financoop_progress_html',
+        $output,
+        $campaign,
+        $atts
+    );
 }
 
-function financoop_render_source_progress($source, $goal, $value, $currency)
-{
-    $label = _x($source, 'source progress label', 'forms-bridge');
-    $value = round(min(100, (int) $value), 2);
+function financoop_render_source_progress(
+    $campaign,
+    $source,
+    $goal,
+    $progress,
+    $currency
+) {
+    $label = _x($source, 'financoop source progress label', 'forms-bridge');
+
+    $amount = round($progress * 0.01 * $goal, 2);
+    $progress = round(min(100, $progress), 2);
     $goal = (int) $goal;
 
     if ($goal === 0) {
@@ -109,20 +154,81 @@ function financoop_render_source_progress($source, $goal, $value, $currency)
     }
 
     ob_start();
-    ?><div class='financoop-progress' data-source="<?php echo esc_attr($source); ?>">
-    <h4 class='wp-block-heading'><?php echo esc_html($label); ?></h4>
-    <p><label><?php echo esc_html(
-        __('Goal', 'forms-bridge')
-    ); ?>: </label><span><?php echo intval($goal); ?> <?php echo esc_html($currency); ?></span></p>
-    <div class="financoop-progress-bar">
-        <label for='<?php echo esc_attr(
-            $source
-        ); ?>-progress'><?php echo esc_html(__('Progress', 'forms-bridge')); ?>: <span><?php echo $value; ?> %</span></label></br>
-        <progress id='<?php echo esc_attr(
-            $source
-        ); ?>-progress' value='<?php echo intval($value); ?>' max='100'><?php echo $value; ?> %</progress>
+    ?><div class="financoop-campaign-progress" data-source="<?php echo esc_attr(
+    $source
+); ?>">
+    <h4 class="wp-block-heading"><?php echo esc_html($label); ?></h4>
+
+	<p class="financoop-campaign-progress-item fraction">
+		<span class="label"><?php echo esc_html(
+      _x('Achieved', 'financoop campaign widget', 'forms-bridge')
+  ); ?>: </span><span class="value"><?php echo number_format(
+    $amount,
+    2,
+    ',',
+    '.'
+); ?> / <span class="value"><?php echo number_format(
+     $goal,
+     2,
+     ',',
+     '.'
+ ); ?> <span class="unit"><?php echo esc_html($currency); ?></span></span>
+	</p>
+
+	<p class="financoop-campaign-progress-item goal">
+		<span class="label"><?php echo esc_html(
+      _x('Goal', 'financoop campaign widget', 'forms-bridge')
+  ); ?>: </span><span class="value"><?php echo number_format(
+    $goal,
+    2,
+    ',',
+    '.'
+); ?> <span class="unit"><?php echo esc_html($currency); ?></span></span>
+	</p>
+
+	<p class="financoop-campaign-progress-item amount">
+		<span class="label"><?php echo esc_html(
+      _x('Received', 'financoop campaign widget', 'forms-bridge')
+  ); ?>: </span><span class="value"><?php echo number_format(
+    $amount,
+    2,
+    ',',
+    '.'
+); ?> <span class="unit"><?php echo esc_html($currency); ?></span></span>
+	</p>
+
+	<p class="financoop-campaign-progress-item percentage">
+        <span class="label"><?php echo esc_html(
+            _x('Progress', 'financoop campaign widget', 'forms-bridge')
+        ); ?>: </span><span class="value"><?php echo number_format(
+    $progress,
+    2,
+    ',',
+    '.'
+); ?> <span class="unit">%</span></span>
+	</p>
+
+    <div class="financoop-campaign-progress-item progress-bar">
+        <progress value='<?php echo intval(
+            $progress
+        ); ?>' max='100'><?php echo $progress; ?> %</progress>
     </div>
-</div><?php return ob_get_clean();
+</div>
+	<?php
+ $output = shortcode_unautop(ob_get_clean());
+
+ return apply_filters(
+     'forms_bridge_financoop_source_progress_html',
+     $output,
+     $campaign,
+     [
+         'source' => $source,
+         'label' => $label,
+         'progress' => $progress,
+         'amount' => $amount,
+         'goal' => $goal,
+     ]
+ );
 }
 
 function financoop_render_campaign_dates($campaign)
@@ -139,50 +245,89 @@ function financoop_render_campaign_dates($campaign)
     $start_date = strtotime($start);
     $end_date = null;
 
-    if ($start_date > time()) {
-        $days_to_start = ($start_date - time()) / (60 * 60 * 24);
+    $now = time();
+
+    if ($start_date > $now) {
+        $days_to_start = ($start_date - $now) / (60 * 60 * 24);
     }
 
     if ($end) {
         $end_date = strtotime($end);
-        $days_to_end = (time() - $end_date) / (60 * 60 * 24);
+        $days_to_end = ($end_date - $now) / (60 * 60 * 24);
     }
 
     if (!$start) {
         return '';
     }
 
-    $output = '<ul class="financoop-campaign-dates">';
+    $output = '<div class="financoop-campaign-dates">';
 
     if ($days_to_start) {
-        $output .= '<li class="financoop-campaign-date days-to-start">';
-        $output .= esc_html(
-            __(sprintf('Starts in %s days', (int) $days_to_start))
-        );
-        $output .= '</li>';
-    } else {
         $output .= sprintf(
-            '<li class="financoop-campaign-date start-date">%s: %s</li>',
-            esc_html(__('Start date', 'forms-bridge')),
-            esc_html($start)
+            '<p class="financoop-campaign-date days-to-start"><span class="label">%s: </span><span class="value">%s</span></p>',
+            esc_html(
+                _x('Days to start', 'financoop campaign widget', 'forms-bridge')
+            ),
+            (int) $days_to_start
         );
     }
 
     if ($end) {
         $output .= sprintf(
-            '<li class="financoop-campaign-date end-date">%s: %s</li>',
-            esc_html(__('End date', 'forms-bridge')),
-            esc_html($end)
+            '<p class="financoop-campaign-date end-date"><span class="label">%s: </span><span class="value">%s</span></p>',
+            esc_html(
+                _x('End date', 'financoop campaign widget', 'forms-bridge')
+            ),
+            esc_html(strftime('%d/%m/%Y', $end_date))
         );
 
-        if (!$days_to_start) {
+        if (!$days_to_start && $days_to_end > 0) {
             $output .= sprintf(
-                '<li class="financoop-campaign-date days-to-end">%s: %s</li>',
-                esc_html(__('Days to end', 'forms-bridge')),
+                '<p class="financoop-campaign-date days-to-end"><span class="label">%s: </span><span class="value">%s</span></p>',
+                esc_html(
+                    _x(
+                        'Days to end',
+                        'financoop campaign widget',
+                        'forms-bridge'
+                    )
+                ),
                 (int) $days_to_end
             );
         }
     }
 
-    return $output . '</ul>';
+    $output .= '</div>';
+
+    return apply_filters(
+        'forms_bridge_financoop_dates_html',
+        $output,
+        $campaign,
+        [
+            'start' => $start,
+            'end' => $end,
+            'days_to_start' => $days_to_start,
+            'days_to_end' => $days_to_end,
+        ]
+    );
+}
+
+function forms_bridge_financoop_shortcode_error($message, $atts)
+{
+    $atts = implode(
+        ' ',
+        array_reduce(
+            ['id', 'backend', 'sources', 'currency'],
+            function ($handle, $attr) use ($atts) {
+                if (isset($atts[$attr])) {
+                    $value = implode(',', (array) $atts[$attr]);
+                    $handle[] = "{$attr}='{$value}'";
+                }
+
+                return $handle;
+            },
+            []
+        )
+    );
+
+    return "[financoop_campaign {$atts}]{$message}[/financoop_campaign]";
 }
