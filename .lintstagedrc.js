bc60cd175e3d6cc8fb83aabbb48bc72df1990ffa
Lucas García <lucas@codeccoop.org>
2025-10-25 01:43:23 +0200

    feat: update lintstaged php command

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -8,13 +8,13 @@ const buildPrettierCommand = (filenames) =>
     .map((f) => path.relative(process.cwd(), f))
     .join(" ")}`;
 
-const buildPhpCsFixerCommand = (filenames) =>
-  `vendor/bin/php-cs-fixer --config=.php-cs-fixer.dist.php fix ${filenames
+const buildPhpCbfCommand = (filenames) =>
+  `vendor/bin/phpcbf ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(" ")}`;
 
 module.exports = {
   "src/**/*.{js,jsx}": [buildEslintCommand],
   "*.{json,js,jsx,html,css}": [buildPrettierCommand],
-  "*.php": [buildPhpCsFixerCommand],
+  "*.php": [buildPhpCbfCommand],
 };
