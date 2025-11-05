9cab5c08992995ece3a74764e94a6b272f6c066f
Lucas García <lucas@codeccoop.org>
2025-11-05 19:23:40 +0100

    feat: downdlad test deps script

---
+++ bin/download-test-deps.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+WORDPRESS_DIR=$1
+
+if [ -z "$WORDPRESS_DIR" ]; then
+	WORDPRESS_DIR='/tmp/wordpress'
+fi
+
+if [ ! -d "$WORDPRESS_DIR" ]; then
+	echo 'Distination path is not a directory'
+	exit 1
+else
+	mkdir -p "$WORDPRESS_DIR/wp-content/mu-plugins"
+fi
+
+URLS=('https://downloads.wordpress.org/plugin/contact-form-7.6.1.3.zip'
+	'https://downloads.wordpress.org/plugin/ninja-forms.3.13.0.zip'
+	'https://www.codeccoop.org/formsbridge/plugins/wpforms.zip'
+	'https://www.codeccoop.org/formsbridge/plugins/gravityforms.zip')
+
+PLUGINS=('contact-form-7' 'gravityforms' 'ninja-forms' 'wpforms')
+
+COUNT=${#PLUGINS[@]}
+
+i=0
+while [ $i -lt $COUNT ]; do
+	URL=${URLS[$i]}
+	PLUGIN=${PLUGINS[$i]}
+
+	curl -sL --connect-time 10 "$URL" >"$TEMP_DIR/$PLUGIN.zip"
+	unzip -qq "$TEMP_DIR/$PLUGIN.zip" -d "$WORDPRESS_DIR/wp-content/mu-plugins"
+
+	i=$((i + 1))
+done
--- bin/test-on-docker.sh
+++ bin/test-on-docker.sh
@@ -17,26 +17,11 @@ echo '✅'
 echo -n 'Install wordpress test suite:                 '
 TMPDIR=/opt bin/install-wp-tests.sh >/dev/null 2>&1
 echo '✅'
-echo 'Download integrations:'
-mkdir -p /tmp/wordpress/wp-content/mu-plugins
-echo -n '  Contact Form 7:                             '
-curl -sL --connect-timeout 10 https://downloads.wordpress.org/plugin/contact-form-7.6.1.3.zip > /tmp/contact-form-7.zip
-unzip -qq /tmp/contact-form-7.zip -d /tmp/wordpress/wp-content/mu-plugins
-echo '✅'
-echo -n '  Ninja Forms:                                '
-curl -sL --connect-timeout 10 https://downloads.wordpress.org/plugin/ninja-forms.3.13.0.zip > /tmp/ninja-forms.zip
-unzip -qq /tmp/ninja-forms.zip -d /tmp/wordpress/wp-content/mu-plugins
-echo '✅'
-echo -n '  WPForms:                                    '
-curl -sL --connect-timeout 10 https://www.codeccoop.org/formsbridge/plugins/wpforms.zip > /tmp/wpforms.zip
-unzip -qq /tmp/wpforms.zip -d /tmp/wordpress/wp-content/mu-plugins
-echo '✅'
-echo -n '  GravityForms:                               '
-curl -sL --connect-timeout 10 https://www.codeccoop.org/formsbridge/plugins/gravityforms.zip > /tmp/gravityforms.zip
-unzip -qq /tmp/gravityforms.zip -d /tmp/wordpress/wp-content/mu-plugins
+echo -n 'Download test dependencies:                   '
+bash bin/download-test-deps.sh
 echo '✅'
 
-echo 'Run tests! 🚀'
+echo 'Run tests!                                    🚀'
 echo
 
 vendor/bin/phpunit
