8c44e28b5e72c414db05eec9f9306c13323d884e
Lucas García <lucas@codeccoop.org>
2026-01-23 00:26:08 +0100

    feat: zoho addons tests

---
+++ tests/addons/test-slack.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Class SlackTest
+ *
+ * @package formsbridge-tests
+ */
+
+use FORMS_BRIDGE\Slack_Form_Bridge;
+
+/**
+ * Slack test case.
+ */
+class SlackTest extends WP_UnitTestCase {
+}
---
+++ tests/addons/test-zulip.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Class ZulipTest
+ *
+ * @package formsbridge-tests
+ */
+
+use FORMS_BRIDGE\Zulip_Form_Bridge;
+
+/**
+ * Zulip test case.
+ */
+class ZulipTest extends WP_UnitTestCase {
+}
