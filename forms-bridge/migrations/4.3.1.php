d6e63bc903c3cd6d08d372f6b1ad413aa1a55b54
Lucas García <lucas@codeccoop.org>
2026-01-28 02:56:21 +0100

    feat: migration 4.3.1

---
+++ forms-bridge/migrations/4.3.1.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Database migration to version 4.0.7
+ *
+ * @package formsbridge
+ */
+
+// phpcs:disable WordPress.Files.FileName
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit();
+}
+
+/**
+ * Migration 4.3.1
+ */
+function forms_bridge_migration_431() {
+	$http = get_option( 'forms-bridge_http', array() ) ?: array(
+		'backends'    => array(),
+		'credentials' => array(),
+	);
+
+	foreach ( $http['credentials'] as &$credential ) {
+		if ( 'Bearer' === $credential['schema'] ) {
+			$credential['schema'] = 'OAuth';
+		}
+	}
+
+	update_option( 'forms-bridge_http', $http );
+}
